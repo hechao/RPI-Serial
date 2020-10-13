6ce5e6305c5d459fd1caad8226621a45942af687
hechao <info@electrodragon.com>
2020-10-13 08:37:24 +0100

    update

---
+++ at.py
@@ -0,0 +1,16 @@
+import serial
+import time 
+
+phone = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1.0)
+
+phone.write('AT\r\n')
+result=phone.read(100)
+print result
+
+#phone.write('AT+CMGF=1\r\n')
+#result=phone.read(100)
+#print result
+
+#phone.write('AT+CMGS="+336xxxxxxxx"\r\n')
+#result=phone.read(100)
+#print result
